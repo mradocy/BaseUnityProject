121373c2fbe227e96905179bffa87d6a0d111a6a
mradocy <mradocy@gmail.com>
2018-06-28 23:22:02 -0400

    Root folders prefaced with underscore, changed commit/update scripts to not create .orig files.

--- Source/Assets/_Code/Stash/CommandScripts/update-stash.cmd
+++ Source/Assets/_Code/Stash/CommandScripts/update-stash.cmd
@@ -1,20 +1,20 @@
-:: Uses KDiff3 to commit code in the Stash folder to BaseUnityProject.
+:: Uses KDiff3 to update code here from the Stash folder in BaseUnityProject.
 
 @echo OFF
 setlocal
 
-:: The relative directory of this Stash folder:
-set SOURCE_DIRECTORY=../.
+:: Absolute directory to update the Stash folder from:
+set SOURCE_DIRECTORY=D:\Mark\Gamedev\Projects\Unity Projects\BaseUnityProject\Source\Assets\_Code\Stash
 
-:: Absolute directory to merge the Stash folder into into:
-set DESTINATION_DIRECTORY=D:\Mark\Gamedev\Projects\Unity Projects\BaseUnityProject\Source\Assets\Code\Stash
+:: The relative directory of this Stash folder:
+set DESTINATION_DIRECTORY=../.
 
 :: The (only) types of files to merge.
 set FILE_PATTERNS=*.cs;*.cmd;*.bat;*.exe;*.txt;*.shader;
 
 
 :: Calling KDiff3 (make sure KDiff3 is installed and contained in %PATH%)
-call kdiff3 "%~dp0%SOURCE_DIRECTORY%" "%DESTINATION_DIRECTORY%" -m --cs "FilePattern=%FILE_PATTERNS%"
+call kdiff3 "%SOURCE_DIRECTORY%" "%~dp0%DESTINATION_DIRECTORY%" -m --cs "FilePattern=%FILE_PATTERNS%" --cs "CreateBakFiles=0"
 
 
 
