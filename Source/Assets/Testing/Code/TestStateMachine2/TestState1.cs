d8aac3ff94d98c4714963b96c215366090a36d30
mradocy <mradocy@gmail.com>
2018-07-06 22:04:19 -0400

    Testing new state machine snippets

---
+++ Source/Assets/Testing/Code/TestStateMachine2/TestState1.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TestStateMachine2 {
+
+    [System.Serializable]
+    public class TestState1 : BaseState {
+
+        public float prop1;
+        public int prop2;
+        [LongLabel]
+        public KeyCode toState2Key;
+
+        public override void onBegin() {
+            Debug.Log("begin state 1");
+        }
+
+        public override void update() {
+            
+            if (UDeb.keyPressed(toState2Key)) {
+                changeState(testStateMachine2.testState2.id);
+            }
+
+        }
+
+        public override void fixedUpdate() {
+            
+        }
+
+        public override void onEnd() {
+            Debug.Log("The end");
+        }
+
+        public override void onDestroy() {
+            Debug.Log("Destroyed");
+        }
+
+    }
+
+}
