391c87fb957bb47d2db6b1413779842509a45c81
mradocy <mradocy@gmail.com>
2018-07-20 20:12:08 -0400

    Started TransformExtensions

---
+++ Source/Assets/_Code/Stash/TransformExtensions.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class TransformExtensions {
+    
+    /// <summary>
+    /// Sets the x component of the transform's local scale.
+    /// </summary>
+    /// <param name="transform"></param>
+    /// <param name="scaleX"></param>
+    public static void SetLocalScaleX(this Transform transform, float scaleX) {
+        transform.localScale = new Vector3(
+            scaleX,
+            transform.localScale.y,
+            transform.localScale.z);
+    }
+
+}
