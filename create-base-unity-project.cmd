91537cb94e179b15d5dbe4ebeed734a4c54af57e
mradocy <mradocy@gmail.com>
2018-09-09 11:24:16 -0400

    Changed create-base-unity-project to rename Unity project to the name of the project

--- create-base-unity-project.cmd
+++ create-base-unity-project.cmd
@@ -1,5 +1,6 @@
 :: Sets up the base essentials of a Unity project.
 :: Uses KDiff3 to copy files over from BaseUnityProject to the current directory.
+:: Note that this must be called from the directory it's contained in.
 
 @echo OFF
 setlocal
@@ -12,25 +13,36 @@ if not exist "%~dp0Builds" mkdir "%~dp0Builds"
 :: Absolute directory containing the BaseUnityProject:
 set SOURCE_DIRECTORY=D:\Mark\Gamedev\Projects\Unity Projects\BaseUnityProject
 
+:: Name of the source's Unity project (the folder that gets opened in Unity):
+set SOURCE_UNITY_PROJECT_NAME=BaseUnityProject
+
 :: The relative directory of this folder:
 set DESTINATION_DIRECTORY=.
 
 :: Directories to ignore:
-set DIRECTORY_ANTI_PATTERNS=Testing;CVS;.deps;.svn;.hg;.git;.vs;Library;library;Temp;temp;Obj;obj;ActionLogs;actionLogs
+set DIRECTORY_ANTI_PATTERNS=Testing;Sandbox;CVS;.deps;.svn;.hg;.git;.vs;Library;library;Temp;temp;Obj;obj;ActionLogs;actionLogs
 
 :: Files to ignore:
-set FILE_ANTI_PATTERNS=Testing.meta;README.txt;README.txt.meta;*.sln;*.csproj;*.unityproj
+set FILE_ANTI_PATTERNS=Testing.meta;Sandbox.meta;README.txt;README.txt.meta;*.sln;*.csproj;*.unityproj
+
+:: Name of this Unity project (taken from parent of the directory that called this command script):
+for %%I in (.) do set UNITY_PROJECT_NAME=%%~nI%%~xI
+
 
 :: Calling KDiff3 (make sure KDiff3 is installed and contained in %PATH%)
 call kdiff3 "%SOURCE_DIRECTORY%" "%~dp0%DESTINATION_DIRECTORY%" -m --cs "FilePattern=*.*" --cs "DirAntiPattern=%DIRECTORY_ANTI_PATTERNS%" --cs "FileAntiPattern=%FILE_ANTI_PATTERNS%" --cs "CreateBakFiles=0"
 
 
+:: Rename unity project
+ren "%SOURCE_UNITY_PROJECT_NAME%" "%UNITY_PROJECT_NAME%"
+
+
 
 :: Ensure folders in Assets have been created (empty folders aren't saved in git)
-if not exist "%~dp0Source\Assets\_Fonts" mkdir "%~dp0Source\Assets\_Fonts"
-if not exist "%~dp0Source\Assets\_Materials" mkdir "%~dp0Source\Assets\_Materials"
-if not exist "%~dp0Source\Assets\_Prefabs" mkdir "%~dp0Source\Assets\_Prefabs"
-if not exist "%~dp0Source\Assets\_Sprites" mkdir "%~dp0Source\Assets\_Sprites"
+if not exist "%UNITY_PROJECT_NAME%\Assets\_Fonts" mkdir "%UNITY_PROJECT_NAME%\Assets\_Fonts"
+if not exist "%UNITY_PROJECT_NAME%\Assets\_Materials" mkdir "%UNITY_PROJECT_NAME%\Assets\_Materials"
+if not exist "%UNITY_PROJECT_NAME%\Assets\_Prefabs" mkdir "%UNITY_PROJECT_NAME%\Assets\_Prefabs"
+if not exist "%UNITY_PROJECT_NAME%\Assets\_Sprites" mkdir "%UNITY_PROJECT_NAME%\Assets\_Sprites"
 
 
 
