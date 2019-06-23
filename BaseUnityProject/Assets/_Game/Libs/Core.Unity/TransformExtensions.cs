22f21bef72654141fbfdb0e601a2b6b07d71218c
mradocy <mradocy@gmail.com>
2019-06-22 21:56:37 -0400

    Create TransformExtensions.cs

---
+++ BaseUnityProject/Assets/_Game/Libs/Core.Unity/TransformExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Unity {
+
+    /// <summary>
+    /// Extension methods for <see cref="Transform"/>.
+    /// </summary>
+    public static class TransformExtensions {
+
+        /// <summary>
+        /// Sets the x component of this transform's local scale.
+        /// </summary>
+        /// <param name="transform">This transform.</param>
+        /// <param name="scaleX">Scale x to set.</param>
+        public static void SetLocalScaleX(this Transform transform, float scaleX) {
+            Vector3 ls = transform.localScale;
+            ls.x = scaleX;
+            transform.localScale = ls;
+        }
+
+    }
+}
