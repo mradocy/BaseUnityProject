ff71f60b5fc80b50d41372f3f6f34c1307ef817b
mradocy <mradocy@gmail.com>
2019-07-20 14:24:56 -0400

    Updated unity version

--- BaseUnityProject/Assets/_Game/Libs/Core.Unity/GameObjectExtensions.cs
+++ BaseUnityProject/Assets/_Game/Libs/Core.Unity/GameObjectExtensions.cs
@@ -13,7 +13,7 @@ namespace Core.Unity {
         /// Gets the component of the given type, but throws an error if the component could not be found.
         /// </summary>
         /// <typeparam name="T">Type of component to search.</typeparam>
-        /// <param name="component">This component to search</param>
+        /// <param name="gameObject">this GameObject</param>
         /// <returns>Component</returns>
         public static T EnsureComponent<T>(this GameObject gameObject) where T : Component {
             T comp = gameObject.GetComponent<T>();
@@ -21,6 +21,18 @@ namespace Core.Unity {
                 throw new System.Exception($"Component {typeof(T)} not found in game object {gameObject}");
             }
             return comp;
+        }
+
+        /// <summary>
+        /// Destroys all components of the given type attached to this gameObject.
+        /// </summary>
+        /// <typeparam name="T">Type of component to destroy.</typeparam>
+        /// <param name="gameObject">this GameObject</param>
+        public static void DestroyComponents<T>(this GameObject gameObject) where T : Component {
+            T[] comps = gameObject.GetComponents<T>();
+            foreach (T comp in comps) {
+                Object.Destroy(comp);
+            }
         }
 
     }
