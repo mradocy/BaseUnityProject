d3d6fa3c70d25f698d5683648f49a3c130082090
mradocy <mradocy@gmail.com>
2019-07-19 23:03:30 -0400

    Create RendererExtensions.cs

---
+++ BaseUnityProject/Assets/_Game/Libs/Core.Unity/Rendering/RendererExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Core.Unity.Rendering {
+
+    /// <summary>
+    /// Extensions for <see cref="Renderer"/>.
+    /// </summary>
+    public static class RendererExtensions {
+
+        /// <summary>
+        /// Ensures that the renderer's main material has a shader with the given name.
+        /// </summary>
+        /// <param name="renderer">This renderer.</param>
+        /// <param name="shaderName">Name of the shader.</param>
+        public static Shader EnsureShader(this Renderer renderer, string shaderName) {
+            Shader shader = renderer?.material?.shader;
+            if (string.Compare(shader?.name, shaderName, true) != 0) {
+                throw new System.Exception($"Renderer's material does not contain shader with the name {shaderName}");
+            }
+            return shader;
+        }
+
+    }
+}
