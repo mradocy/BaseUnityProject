3054f772109d51d6d5e02a292802d6d733469402
mradocy <mradocy@gmail.com>
2019-08-15 21:50:19 -0400

    Create GraphicExtensions.cs

---
+++ BaseUnityProject/Assets/_Game/Libs/Core.Unity/UI/GraphicExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Core.Unity.UI {
+
+    /// <summary>
+    /// Extensions for <see cref="Graphic"/>.
+    /// </summary>
+    public static class GraphicExtensions {
+
+        /// <summary>
+        /// Ensures that the graphic's main material has a shader with the given name.
+        /// </summary>
+        /// <param name="graphic">This graphic.</param>
+        /// <param name="shaderName">Name of the shader.</param>
+        public static Shader EnsureShader(this Graphic graphic, string shaderName) {
+            Shader shader = graphic?.material?.shader;
+            if (string.Compare(shader?.name, shaderName, true) != 0) {
+                throw new System.Exception($"Graphic's material does not contain shader with the name {shaderName}");
+            }
+            return shader;
+        }
+
+    }
+}
