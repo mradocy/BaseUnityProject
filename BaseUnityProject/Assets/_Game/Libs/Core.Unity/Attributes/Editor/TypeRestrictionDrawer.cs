cc47178bd7f04507a41914511206a014532ee50f
mradocy <mradocy@gmail.com>
2019-10-18 22:43:29 -0400

    Added type restriction attribute

---
+++ BaseUnityProject/Assets/_Game/Libs/Core.Unity/Attributes/Editor/TypeRestrictionDrawer.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Unity;
+using UnityEditor;
+
+namespace Core.Unity.Attributes {
+
+    [CustomPropertyDrawer(typeof(TypeRestrictionAttribute))]
+    public class TypeRestrictionPropertyDrawer : PropertyDrawer {
+
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+            // validate inputs - could draw a 'fail' message if not validated correctly
+            if (property.propertyType != SerializedPropertyType.ObjectReference)
+                return;
+
+            TypeRestrictionAttribute attribute = this.attribute as TypeRestrictionAttribute;
+            if (attribute == null)
+                return;
+
+            // draw
+            EditorGUI.BeginChangeCheck();
+            UnityEngine.Object obj = EditorGUI.ObjectField(position, label, property.objectReferenceValue, typeof(UnityEngine.Object), attribute.AllowSceneObjects);
+            if (EditorGUI.EndChangeCheck()) {
+                if (obj == null) {
+                    property.objectReferenceValue = null;
+                } else {
+                    System.Type type = obj.GetType();
+                    if (!attribute.Type.IsAssignableFrom(type)) {
+                        GameObject go = obj as GameObject;
+                        Component comp = obj as Component;
+                        if (go != null) {
+                            obj = go.GetComponent(attribute.Type);
+                        } else if (comp != null) {
+                            obj = comp.GetComponent(attribute.Type);
+                        } else {
+                            obj = null;
+                        }
+                    }
+
+                    property.objectReferenceValue = obj;
+                }
+            }
+        }
+
+    }
+}
---
+++ BaseUnityProject/Assets/_Game/Libs/Core.Unity/Attributes/TypeRestrictionAttribute.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Unity;
+
+namespace Core.Unity.Attributes {
+
+    /// <summary>
+    /// Restricts the type (e.g. an interface) of a component that can be set to a field in the inspector.
+    /// </summary>
+    public class TypeRestrictionAttribute : PropertyAttribute {
+
+        public TypeRestrictionAttribute(System.Type type, bool allowSceneObjects = true) {
+            if (type == null)
+                throw new System.ArgumentNullException(nameof(type));
+
+            this.Type = type;
+            this.AllowSceneObjects = allowSceneObjects;
+        }
+
+        public System.Type Type { get; }
+
+        public bool AllowSceneObjects { get; }
+
+    }
+}
