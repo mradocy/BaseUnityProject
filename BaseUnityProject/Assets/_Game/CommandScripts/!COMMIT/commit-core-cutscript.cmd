ef5044410fa6c3052722b30c50a168f1088c43b3
mradocy <mradocy@gmail.com>
2020-02-26 21:06:12 -0500

    Update commit-core-cutscript.cmd

--- BaseUnityProject/Assets/_Game/CommandScripts/!COMMIT/commit-core-cutscript.cmd
+++ BaseUnityProject/Assets/_Game/CommandScripts/!COMMIT/commit-core-cutscript.cmd
@@ -8,15 +8,18 @@ pushd "%~dp0"
 set SOURCE_DIRECTORY=..\..\Libs\Core.CutScript
 
 :: Directory of the CutScript project:
-set DESTINATION_DIRECTORY=%GAMEDEV_SOURCE_TOOLS%\Core.CutScript\Core.CutScript
+set DESTINATION_DIRECTORY=%GAMEDEV_SOURCE_TOOLS%\CutScript\Core.CutScript
 
 :: The (only) types of files to merge.
 set FILE_PATTERNS=*.cs;*.cmd;*.bat;*.exe;*.txt;*.shader;*.md;
 
+:: Directories to ignore:
+set DIR_IGNORE=Obj;obj;Bin;bin
+
 
 :: Calling KDiff3 (make sure KDiff3 is installed and contained in %PATH%)
 :: KDiff3 command line: http://kdiff3.sourceforge.net/doc/documentation.html
-call kdiff3 "%SOURCE_DIRECTORY%" "%DESTINATION_DIRECTORY%" -m --cs "FilePattern=%FILE_PATTERNS%" --cs "CreateBakFiles=0"
+call kdiff3 "%SOURCE_DIRECTORY%" "%DESTINATION_DIRECTORY%" -m --cs "FilePattern=%FILE_PATTERNS%" --cs "DirAntiPattern=%DIR_IGNORE%" --cs "CreateBakFiles=0"
 
 
 popd
