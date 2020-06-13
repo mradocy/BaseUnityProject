c6a5db9f0933209b3e6a9aa074610c90ae7a8aba
mradocy <mradocy@gmail.com>
2020-06-13 08:02:33 -0400

    Refactored menus

--- BaseUnityProject/Assets/_Game/Libs/Core.Unity/UI/Options/OptionsMenuBase.cs
+++ BaseUnityProject/Assets/_Game/Libs/Core.Unity/UI/Options/OptionsMenuBase.cs
@@ -1,98 +1,90 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 
 namespace Core.Unity.UI.Options {
 
     /// <summary>
-    /// Base class for an options menu
+    /// Abstract base class for an options menu.
+    /// Does not reference controls.
     /// </summary>
     public abstract class OptionsMenuBase : MonoBehaviour {
 
-        #region Inspector Fields
-
-        [Header("OptionsMenuBase Children")]
-
-        [SerializeField]
-        [Tooltip("Transform to put the option controls in.")]
-        private VerticalLayoutGroup _controlGroup = null;
-
-        #endregion
-
-        #region Properties
+        #region Public Properties
 
         /// <summary>
-        /// Gets the index of the control that is currently selected.  -1 means no control is selected.
+        /// Gets the index of the option that is currently selected.  -1 means no option is selected.
         /// </summary>
         public int SelectedIndex { get; private set; } = -1;
 
         /// <summary>
-        /// Gets the control that is currently selected, or null if no control is selected.
+        /// Gets the timestamp (in <see cref="Time.unscaledTime"/>) when the selected option changed.
         /// </summary>
-        public OptionControlBase SelectedControl {
-            get {
-                return this.GetOptionControl(this.SelectedIndex);
-            }
-        }
+        public float SelectedOptionChangeTime { get; private set; } = 0;
 
         /// <summary>
         /// Gets the number of options.
         /// </summary>
-        public int OptionsCount {
-            get { return _optionControls.Count; }
-        }
-
-        /// <summary>
-        /// Gets the timestamp (in <see cref="Time.unscaledTime"/>) when the selected option changed.
-        /// </summary>
-        public float SelectedOptionTime { get; private set; } = 0;
+        public int OptionsCount => _options?.Count ?? 0;
 
         #endregion
 
-        #region Methods
-
-        /// <summary>
-        /// Gets the option control at the given index.  Returns null if the index is invalid.
-        /// </summary>
-        /// <param name="optionIndex">Option index.</param>
-        /// <returns>Option control.</returns>
-        public OptionControlBase GetOptionControl(int optionIndex) {
-            if (optionIndex < 0 || optionIndex >= this.OptionsCount)
-                return null;
-
-            return _optionControls[optionIndex];
-        }
+        #region Public Methods
 
         /// <summary>
         /// Selects the option control at the given index.  Does nothing if the index is the same as the current index.
         /// </summary>
         /// <param name="optionIndex">Index of the option to select.  Input -1 to select no option.</param>
-        public void SelectOption(int optionIndex) {
-            if (optionIndex < 0 || optionIndex >= this.OptionsCount) {
-                optionIndex = -1;
+        /// <param name="animate">If the option selection should be animated.  False may be useful for setting the first option selected.</param>
+        public void SelectOption(int optionIndex, bool animate) {
+            if (optionIndex < -1 || optionIndex >= this.OptionsCount) {
+                Debug.LogError($"Option index {optionIndex} is invalid.  There are only {this.OptionsCount} options.");
+                return;
             }
 
             if (optionIndex == this.SelectedIndex)
                 return;
 
             int prevIndex = this.SelectedIndex;
-            OptionControlBase prevControl = this.SelectedControl;
-            if (prevControl != null) {
-                prevControl.Unselect();
-            }
-            this.SelectedOptionTime = Time.unscaledTime;
+            this.SelectedOptionChangeTime = Time.unscaledTime;
             this.SelectedIndex = optionIndex;
-            OptionControlBase selectedControl = this.SelectedControl;
-            if (selectedControl != null) {
-                selectedControl.Select();
-            }
-            this.OnSelectionChanged(prevIndex, this.SelectedIndex);
+
+            this.OnSelectionChanged(prevIndex, this.SelectedIndex, animate);
+        }
+
+        /// <summary>
+        /// Gets the <see cref="IOption"/> at the given index.
+        /// </summary>
+        /// <param name="optionIndex">Option index</param>
+        /// <returns>Option</returns>
+        public IOption GetOption(int optionIndex) {
+            if (optionIndex < 0 || optionIndex >= this.OptionsCount)
+                return null;
+
+            return _options[optionIndex];
+        }
+
+        /// <summary>
+        /// Gets the option at the given index cast as a <see cref="IButtonOption"/>.
+        /// </summary>
+        /// <param name="optionIndex">Option index</param>
+        /// <returns>Button option</returns>
+        public IButtonOption GetButtonOption(int optionIndex) {
+            return this.GetOption(optionIndex) as IButtonOption;
+        }
+
+        /// <summary>
+        /// Gets the option at the given index cast as a <see cref="IPropertyOption"/>.
+        /// </summary>
+        /// <param name="optionIndex">Option index</param>
+        /// <returns>Property option</returns>
+        public IPropertyOption GetPropertyOption(int optionIndex) {
+            return this.GetOption(optionIndex) as IPropertyOption;
         }
 
         #endregion
 
-        #region Protected Properties to be Overridden
+        #region Protected Abstract Properties
 
         /// <summary>
         /// Gets a reference to a ui input handler.
@@ -111,50 +103,24 @@ namespace Core.Unity.UI.Options {
         /// <remarks>0.1f is a good value to use.</remarks>
         protected abstract float HoldChangePeriod { get; }
 
-        /// <summary>
-        /// When true, if there is a <see cref="ModalWindow"/> attached to this gameObject or a parent and it's disabled,
-        /// then the Update() method for this menu (i.e. the user input) will also be disabled.
-        /// </summary>
-        protected virtual bool DisableUpdateIfParentModalWindowIsDisabled { get; } = true;
-
         #endregion
 
-        #region Protected Methods to be Overridden
-
-        /// <summary>
-        /// Initializes the list of option models for controls to be made from.
-        /// </summary>
-        /// <returns>Option models.</returns>
-        protected abstract IList<IOption> InitOptions();
-
-        /// <summary>
-        /// Creates a new <see cref="OptionControlBase"/> game object with the given option.  Used to specify custom option control implementations for each option.
-        /// </summary>
-        /// <param name="option">Option to create a control from.</param>
-        /// <returns>Option control.</returns>
-        protected abstract OptionControlBase CreateControl(IOption option);
-
-        /// <summary>
-        /// Destroys the given control.  By default this uses Unity's Destroy() method, can be overridden to recycle instead.
-        /// </summary>
-        /// <param name="control">Control to destroy.</param>
-        protected virtual void DestoryControl(OptionControlBase control) {
-            Destroy(control.gameObject);
-        }
+        #region Protected Virtual Methods
 
         /// <summary>
         /// Called when the selection changes.
         /// </summary>
         /// <param name="prevIndex">The previous <see cref="SelectedIndex"/>.</param>
-        /// <param name="currentIndex">The current <see cref="SelectedIndex"/>.</param>
-        protected virtual void OnSelectionChanged(int prevIndex, int currentIndex) { }
+        /// <param name="currentIndex">The current <see cref="SelectedIndex"/>.  -1 for no option selected.</param>
+        /// <param name="animate">If the selection change should be animated.  False may be useful for setting the first option selected.</param>
+        protected virtual void OnSelectionChanged(int prevIndex, int currentIndex, bool animate) { }
 
         /// <summary>
-        /// Called when the value of a property option is decremented or incremented.
+        /// Called when the value of a <see cref="IPropertyOption"/> is decremented or incremented.
         /// </summary>
-        /// <param name="propertyOptionControl">The control of the option changed.</param>
+        /// <param name="optionIndex">The index of the property option changed.</param>
         /// <param name="incremented">True if the value was incremented, false if the value was decremented.</param>
-        protected virtual void OnPropertyValueChanged(PropertyOptionControlBase propertyOptionControl, bool incremented) { }
+        protected virtual void OnPropertyValueChanged(int optionIndex, bool incremented) { }
 
         /// <summary>
         /// Called when the cancel button is pressed.
@@ -171,42 +137,34 @@ namespace Core.Unity.UI.Options {
         /// </summary>
         protected virtual void OnUpdate() { }
 
+        /// <summary>
+        /// Called by Unity when this object is destroyed.
+        /// </summary>
+        protected virtual void OnDerivedDestroy() { }
+
         #endregion
 
-        #region Protected Methods
+        #region Protected Helper Methods
 
         /// <summary>
-        /// Creates (or recreates) the option controls for this menu.
+        /// Sets the options of this menu.
         /// </summary>
-        protected void CreateOptionControls() {
-            // create option models
-            this.ClearOptions();
-            _optionModels.AddRange(this.InitOptions());
-            
-            // create controls
-            foreach (IOption optionModel in _optionModels) {
-                OptionControlBase optionControl = this.CreateControl(optionModel);
-                if (optionControl == null) {
-                    Debug.LogError("Error when creating option control");
-                    continue;
-                }
-                RectTransform rectTransform = optionControl.EnsureComponent<RectTransform>();
-                rectTransform.anchoredPosition = Vector2.zero;
-                rectTransform.SetParent(_controlGroup.transform);
-                _optionControls.Add(optionControl);
+        /// <param name="options">Options</param>
+        protected virtual void SetOptions(IList<IOption> options) {
+            if (_options == options)
+                return;
+
+            _options = options;
+
+            // ensure selected option index is valid
+            if (this.SelectedIndex >= this.OptionsCount) {
+                this.SelectedIndex = -1;
             }
         }
 
-        /// <summary>
-        /// Destroys all options and option controls.
-        /// </summary>
-        protected void ClearOptions() {
-            foreach (OptionControlBase optionControl in _optionControls) {
-                this.DestoryControl(optionControl);
-            }
-            _optionControls.Clear();
-            _optionModels.Clear();
-        }
+        #endregion
+
+        #region Unity Methods
 
         /// <summary>
         /// Called by Unity when the script instance is being loaded.
@@ -215,19 +173,15 @@ namespace Core.Unity.UI.Options {
             _parentModalWindow = this.GetComponentInParent<ModalWindow>();
 
             this.OnAwake();
-
-            this.CreateOptionControls();
-            this.SelectOption(0);
         }
 
         /// <summary>
         /// Called by Unity every frame, if the MonoBehaviour is enabled.
         /// </summary>
         protected void Update() {
-            if (this.DisableUpdateIfParentModalWindowIsDisabled) {
-                if (_parentModalWindow != null && !_parentModalWindow.enabled)
-                    return;
-            }
+            // disable update if parent modal window is disabled
+            if (_parentModalWindow != null && !_parentModalWindow.enabled)
+                return;
 
             float timestamp = Time.unscaledTime;
             if (this.UIInput.IsLeftPressed) {
@@ -237,42 +191,46 @@ namespace Core.Unity.UI.Options {
                 _rightPressedTime = timestamp;
             }
 
-            OptionControlBase selectedOptionControl = this.SelectedControl;
-            if (this.OptionsCount > 0) {
+            if (_options.Count > 0) {
                 if (this.UIInput.IsSubmitPressed) {
-                    // execute the selected option
-                    ButtonOptionControlBase buttonOptionControl = selectedOptionControl as ButtonOptionControlBase;
-                    if (buttonOptionControl != null && buttonOptionControl.ButtonOption.CanExecute) {
-                        buttonOptionControl.Execute();
+                    // execute the selected button option
+                    IButtonOption buttonOption = this.GetButtonOption(this.SelectedIndex);
+                    if (buttonOption != null && buttonOption.CanExecute) {
+                        buttonOption.Execute();
                     }
                 } else if (this.UIInput.IsCancelPressed) {
                     // call derived cancel method
                     this.OnCancelPressed();
                 } else if (this.UIInput.IsDownPressed) {
                     // increment selected index
-                    if (this.SelectedIndex < 0 || this.SelectedIndex >= this.OptionsCount - 1) {
-                        this.SelectOption(0);
+                    if (this.SelectedIndex < 0 || this.SelectedIndex >= this.OptionsCount) {
+                        // failsafe
+                        this.SelectOption(0, false);
+                    } else if (this.SelectedIndex >= this.OptionsCount - 1) {
+                        // wrap around
+                        this.SelectOption(0, true);
                     } else {
-                        this.SelectOption(this.SelectedIndex + 1);
+                        this.SelectOption(this.SelectedIndex + 1, true);
                     }
                 } else if (this.UIInput.IsUpPressed) {
                     // decrement selected index
                     if (this.SelectedIndex < 0 || this.SelectedIndex >= this.OptionsCount) {
-                        this.SelectOption(0);
+                        // failsafe
+                        this.SelectOption(0, false);
                     } else if (this.SelectedIndex == 0) {
-                        this.SelectOption(this.OptionsCount - 1);
+                        this.SelectOption(this.OptionsCount - 1, true);
                     } else {
-                        this.SelectOption(this.SelectedIndex - 1);
+                        this.SelectOption(this.SelectedIndex - 1, true);
                     }
-                } else if (this.UIInput.IsLeftHeld && selectedOptionControl != null) {
-                    PropertyOptionControlBase propertyOptionControl = selectedOptionControl as PropertyOptionControlBase;
-                    if (propertyOptionControl != null && propertyOptionControl.PropertyOption.CanDecrement) {
-                        // decrement property option if conditions are met
+                } else if (this.UIInput.IsLeftHeld) {
+                    // decrement the selected property option
+                    IPropertyOption propertyOption = this.GetPropertyOption(this.SelectedIndex);
+                    if (propertyOption != null && propertyOption.CanDecrement) {
                         bool decrement = false;
                         if (this.UIInput.IsLeftPressed) {
                             // pressing the left button to decrement
                             decrement = true;
-                        } else if (propertyOptionControl.PropertyOption.CanHoldChange) {
+                        } else if (propertyOption.CanHoldChange) {
                             // holding the left button to decrement
                             if (timestamp > _leftPressedTime + this.HoldChangeWaitDuration &&
                                 timestamp > _decrementTime + this.HoldChangePeriod) {
@@ -281,20 +239,20 @@ namespace Core.Unity.UI.Options {
                         }
 
                         if (decrement) {
-                            propertyOptionControl.Decrement();
+                            propertyOption.Decrement();
                             _decrementTime = timestamp;
-                            this.OnPropertyValueChanged(propertyOptionControl, false);
+                            this.OnPropertyValueChanged(this.SelectedIndex, false);
                         }
                     }
-                } else if (this.UIInput.IsRightHeld && selectedOptionControl != null) {
-                    PropertyOptionControlBase propertyOptionControl = selectedOptionControl as PropertyOptionControlBase;
-                    if (propertyOptionControl != null && propertyOptionControl.PropertyOption.CanIncrement) {
-                        // increment property option if conditions are met
+                } else if (this.UIInput.IsRightHeld) {
+                    // increment the selected property option
+                    IPropertyOption propertyOption = this.GetPropertyOption(this.SelectedIndex);
+                    if (propertyOption != null && propertyOption.CanIncrement) {
                         bool increment = false;
                         if (this.UIInput.IsRightPressed) {
                             // pressing the right button to increment
                             increment = true;
-                        } else if (propertyOptionControl.PropertyOption.CanHoldChange) {
+                        } else if (propertyOption.CanHoldChange) {
                             // holding the right button to increment
                             if (timestamp > _rightPressedTime + this.HoldChangeWaitDuration &&
                                 timestamp > _incrementTime + this.HoldChangePeriod) {
@@ -303,9 +261,9 @@ namespace Core.Unity.UI.Options {
                         }
 
                         if (increment) {
-                            propertyOptionControl.Increment();
+                            propertyOption.Increment();
                             _incrementTime = timestamp;
-                            this.OnPropertyValueChanged(propertyOptionControl, true);
+                            this.OnPropertyValueChanged(this.SelectedIndex, true);
                         }
                     }
                 }
@@ -318,15 +276,14 @@ namespace Core.Unity.UI.Options {
         /// Called by Unity when this object is destroyed.
         /// </summary>
         protected void OnDestroy() {
-            this.ClearOptions();
+            this.OnDerivedDestroy();
         }
 
         #endregion
 
-        #region Private Fields
+        #region Private
 
-        private List<IOption> _optionModels = new List<IOption>();
-        private List<OptionControlBase> _optionControls = new List<OptionControlBase>();
+        private IList<IOption> _options = null;
 
         private float _leftPressedTime = -9999;
         private float _rightPressedTime = -9999;
