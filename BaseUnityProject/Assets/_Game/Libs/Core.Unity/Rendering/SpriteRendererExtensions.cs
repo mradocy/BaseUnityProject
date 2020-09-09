95160b4d0b223a10c957d10f8a7e5de5720716a6
mradocy <mradocy@gmail.com>
2020-09-09 16:44:45 -0400

    Added SpriteRendererExtensions, removed ControllerListenerBase

---
+++ BaseUnityProject/Assets/_Game/Libs/Core.Unity/Rendering/SpriteRendererExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Core.Unity;
+
+namespace Core.Unity.Rendering {
+
+    /// <summary>
+    /// Extensions for <see cref="SpriteRenderer"/>.
+    /// </summary>
+    public static class SpriteRendererExtensions {
+
+        /// <summary>
+        /// Updates the color of this sprite renderer to the given alpha value.
+        /// </summary>
+        public static void SetAlpha(this SpriteRenderer spriteRenderer, float alpha) {
+            Color c = spriteRenderer.color;
+            c.a = alpha;
+            spriteRenderer.color = c;
+        }
+
+    }
+}
